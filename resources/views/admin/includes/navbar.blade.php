14d7d0880578a1c7bab4218c3f61cc7c48ccc3ce
prateekpppp <prateekpppp4@gmail.com>
2026-01-19 18:16:35 +0530

    up

--- resources/views/admin/includes/navbar.blade.php
+++ resources/views/admin/includes/navbar.blade.php
@@ -47,7 +47,7 @@
                                 {{-- <span>{{isset($appdata->status) ? ($appdata->status ? 'Admin' : 'User') : ''}}</span> --}}
                             </div>
                             <div class="admin-img">
-                                <img src="img/figure/admin.jpg" alt="Admin">
+                                <img src="{{$currentuser->status > '2' ? asset(img/figure/admin.jpg)}}" alt="Admin">
                             </div>
                         </a>
                         {{-- @if($sessions) --}}
