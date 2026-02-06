33585fad6e988cec624bd0adc5ed958a4f9f06a7
prateekpppp <prateekpppp4@gmail.com>
2026-02-06 17:02:32 +0530

    up

--- resources/views/staff/includes/sidebar.blade.php
+++ resources/views/staff/includes/sidebar.blade.php
@@ -29,9 +29,6 @@
                             <a href="{{route('staff.pages.staffSalary')}}" class="nav-link"><i class="fas fa-angle-right"></i><span>Salary Report</span></a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{route('staff.pages.expanse')}}" class="nav-link"><i class="fas fa-angle-right"></i><span>Expense</span></a>
-                        </li>
-                        <li class="nav-item">
                             <a href="{{route('staff.pages.applicationletter')}}" class="nav-link"><i class="fas fa-angle-right"></i><span>Application Letter</span></a>
                         </li>
                     </ul>
