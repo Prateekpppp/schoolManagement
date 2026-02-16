b903e12a2f785412d8b2734d57a5267509a62426
Prateekpppp <prateekpppp4gmail.com>
2026-02-16 20:50:00 +0530

    up

--- app/Http/Controllers/StaffAttendanceController.php
+++ app/Http/Controllers/StaffAttendanceController.php
@@ -55,7 +55,7 @@ class StaffAttendanceController extends Controller
 
             // dd($distance);
 
-            if($distance > 30){
+            if($distance > 100){
                 return response()->json([
                     'message'=>'Not under the required school area!',
                     'response_code'=> '405'
